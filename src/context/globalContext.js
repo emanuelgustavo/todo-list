e6936ddae9b0ed60018b6be8a277df79719e3153
Emanuel Gustavo Vierne <emanuelgustavovierne@gmail.com>
2022-04-27 07:16:25 -0300

    Add functions

--- src/context/globalContext.js
+++ src/context/globalContext.js
@@ -4,5 +4,9 @@ export default React.createContext({
   settings: {},
   taskList: [],
   updateSettings: (newSettings) => {},
-  addNewTask: (newTask) => {}
+  addNewTask: (newTask) => {},
+  updateRestStatus: (task) => {},
+  updateTaskDoneStatus: (task) => {},
+  updateRestDoneStatus: (task) => {},
+  updateFinishedTaskStatus: (task) => {}
 });
